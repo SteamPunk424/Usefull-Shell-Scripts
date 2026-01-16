a6f8a45a704d158ac32f419a87e1639467f71cdf
William James Schleppegrell <32554693+SteamPunk424@users.noreply.github.com>
2026-01-16 04:51:32 -0600

    Added ease of use to the custom ID wipe script

--- LocalNetworkIdentityWiper/Identity Wipe(custom).sh
+++ LocalNetworkIdentityWiper/Identity Wipe(custom).sh
@@ -1,15 +1,25 @@
-#/!/bin/sh
+#!/bin/sh
+echo "Enter the MAC address you want to set (format: XX:XX:XX:XX:XX:XX):"
+read NEW_MAC
+if ! echo "$NEW_MAC" | grep -Eq '^([0-9A-Fa-f]{2}:){5}[0-9A-Fa-f]{2}$'; then
+  echo "[-] Invalid MAC address format."
+  exit 1
+fi
+
 sudo ifconfig eth0 down
 sudo ifconfig wlan0 down
 sudo ifconfig docker0 down
-sudo macchanger --mac=0C:B9:37:6C:51:68 eth0
-sudo macchanger --mac=0C:B9:37:6C:51:68 wlan0
-sudo macchanger --mac=0C:B9:37:6C:51:68 docker0
+
+sudo macchanger --mac="$NEW_MAC" eth0
+sudo macchanger --mac="$NEW_MAC" wlan0
+sudo macchanger --mac="$NEW_MAC" docker0
+
 sudo ifconfig eth0 up
 sudo ifconfig wlan0 up
 sudo ifconfig docker0 up
+
 clear
-echo Your new identity is ready
-echo Restarting Your Computer in 10 seconds
+echo "Your new identity is ready."
+echo "Restarting your computer in 10 seconds..."
 sleep 10
 sudo shutdown -r now
